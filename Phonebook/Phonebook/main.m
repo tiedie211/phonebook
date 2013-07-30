e01af4937ab965fbcd5aba4513315e179368ba13
Tyler D <tchristiand@gmail.com>
2013-07-30 10:59:03 -0700

    wont run class methods, or i wrote it wrong

--- Phonebook/Phonebook/main.m
+++ Phonebook/Phonebook/main.m
@@ -15,12 +15,23 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        Phonebook *phonebook = [[Phonebook alloc] init];
-        [Phonebook addEntry:@"Tyler"
+        Phonebook *entry1 = [[Phonebook alloc] init];
+        [entry1 addEntry:@"Tyler"
                       Email:@"tchristiand@gmail.com"
                 SocialMedia:@"Facebook"
                 PhoneNumber:2088182575];
+        Phonebook *entry2 = [[Phonebook alloc] init];
+        [entry2 addEntry:@"Cole"
+                      Email:@"cdLewis@gmail.com"
+                SocialMedia:@"Facebook"
+                PhoneNumber:5098172121];
         
+        NSArray *entries = [NSArray arrayWithObjects:entry1, entry2, nil];
+        
+        for (Phonebook *p in entries)
+        {
+            NSLog(@"\nName: %@, Email: %@, Media: %@, Number: %lu\n", [p name], [p email], [p media], [p number]);
+        }
     }
     return 0;
 }
